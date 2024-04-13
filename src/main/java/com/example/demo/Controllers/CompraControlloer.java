250499f50170143c2543b1e646ed219326db89e8
rguindi <raul_fv@hotmail.com>
2024-04-14 00:56:07 +0200

    index comprobar Stock

--- src/main/java/com/example/demo/Controllers/CompraControlloer.java
+++ src/main/java/com/example/demo/Controllers/CompraControlloer.java
@@ -2,12 +2,15 @@ package com.example.demo.Controllers;
 
 
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.Entities.Pedido;
+import com.example.demo.Entities.Producto;
 import com.example.demo.Repositories.PedidoRepository;
 import com.example.demo.services.CarritoService;
 import com.example.demo.services.CompraServicio;
@@ -30,11 +33,17 @@ public class CompraControlloer {
 	
 	
 	@GetMapping("/procesarCompra")
-	public String editar(HttpServletRequest request) {
+	public String editar(HttpServletRequest request, Model model) {
 		if(!carritoService.userIsLoged(request)) return "redirect:/login";
 		
-		//Pendiente controlar Stock
 		
+		//Si no hay Stock regeneramos el stock en el carrito y volvemos al resumen
+		if(!compraServicio.modificarStock(request)) {
+			HashMap<Producto, Integer> carrito = (HashMap<Producto, Integer>) request.getSession().getAttribute("carrito");
+			carrito = compraServicio.regenerarStock(carrito);
+			model.addAttribute("errorStock", "Error al revisar el Stock del producto. Compruebe su cesta.");
+			return "redirect:/resumenPedido";
+		}
 		
 		Pedido pedido = compraServicio.generaPedido(request);
 		pedidoRepository.save(pedido);
