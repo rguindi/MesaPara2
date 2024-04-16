7b202af27db575d9d46a013861785d989a2d60be
rferrero <rferrero@serbatic-05429.VASS.INET>
2024-04-16 21:36:56 +0200

    soliitar cancelacion

--- src/main/java/com/example/demo/Controllers/pedidosController.java
+++ src/main/java/com/example/demo/Controllers/pedidosController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.example.demo.Entities.Producto;
@@ -30,6 +32,15 @@ public class pedidosController {
 	@GetMapping("/misPedidos")
 	public String misPedidos(Model model, @SessionAttribute("usuario") Usuario usuario) {
 		
+		model.addAttribute("listaPedidos", pedidoRepository.buscarPorIdUsuario(usuario.getId()));
+		
+		return "/misPedidos";
+	}
+	
+	@PostMapping("/cancelarPedido")
+	public String cancelarPedido(Model model, @SessionAttribute("usuario") Usuario usuario, @RequestParam("id") Long id) {
+		
+		pedidoRepository.cambiarEstadoAPC(id);
 		model.addAttribute("listaPedidos", pedidoRepository.buscarPorIdUsuario(usuario.getId()));
 		
 		return "/misPedidos";
