2f1d60dd22486d6ea49bdaafe543f56829ddbc55
rguindi <raul_fv@hotmail.com>
2024-05-03 12:30:41 +0200

    Corrigiendo problema en Java Mail Sender

--- src/main/java/curso/java/tienda/Config/conf.java
+++ src/main/java/curso/java/tienda/Config/conf.java
@@ -1,12 +1,17 @@
 package curso.java.tienda.Config;
 
 import org.springframework.context.MessageSource;
+
+import java.util.Properties;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import curso.java.tienda.upload.storage.StorageProperties;
@@ -58,5 +63,23 @@ public class conf {
 		bean.setValidationMessageSource(messageSource());
 		return bean;
 	}
+	
+	
+	@Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587); // Puerto estándar para SMTP
+        mailSender.setUsername("raulserbatic@gmail.com");
+        mailSender.setPassword("tmui ubpk kdzb idwt");
+
+        // Configuración para SMTP con autenticación y TLS
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true"); // Habilitar autenticación
+        props.put("mail.smtp.starttls.enable", "true"); // Habilitar TLS
+
+        return mailSender;
+    
+	    }
 
 }
